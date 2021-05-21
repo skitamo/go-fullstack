116073490957ace6c34106e6ee04de632d41eeef
Amaury Le Meur <amaury.lemeur@gmail.com>
2021-05-21 17:06:18 +0200

    Résolution de tous les problèmes et modification de la route post pour prendre en compte les fichiers

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
 const stuffCtrl = require('../controllers/stuff');
+const multer = require('../middleware/multer-config');
 
-router.get('/', stuffCtrl.getAllStuff);
-router.post('/', stuffCtrl.createThing);
-router.get('/:id', stuffCtrl.getOneThing);
-router.put('/:id', stuffCtrl.modifyThing);
-router.delete('/:id', stuffCtrl.deleteThing);
+router.post('/', auth, multer, stuffCtrl.createThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.get('/', auth, stuffCtrl.getAllThings);
 
 module.exports = router;
