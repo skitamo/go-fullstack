00950b4a0c619bb559661bf6924c14ae218891ae
Amaury Le Meur <amaury.lemeur@gmail.com>
2021-05-21 12:58:35 +0200

    Modification d'un Thing

--- backend/app.js
+++ backend/app.js
@@ -37,6 +37,12 @@ app.get('/api/stuff/:id', (req, res, next) => {
     .catch(error => res.status(404).json({ error }));
 });
 
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
 app.use('/api/stuff', (req, res, next) => {
   Thing.find()
   .then(things => res.status(200).json(things))
