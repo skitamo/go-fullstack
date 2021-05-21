961d293c6791ee611d8af3c435d1c7e9a9fbb724
Amaury Le Meur <amaury.lemeur@gmail.com>
2021-05-21 13:03:14 +0200

    Suppresion d'un Thing

--- backend/app.js
+++ backend/app.js
@@ -43,6 +43,12 @@ app.put('/api/stuff/:id', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
 app.use('/api/stuff', (req, res, next) => {
   Thing.find()
   .then(things => res.status(200).json(things))
